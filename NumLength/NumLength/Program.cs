d0095c08f2ceb2e476db97d8b44be17f1035b354
Aysu Memmedova <aysu.memmedova2270@uni.edu.az>
2022-03-07 17:44:55 +0400

    first commit

---
+++ NumLength/NumLength/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+namespace NumLength
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("NumLength metodu ucun 1");
+            Console.WriteLine("Min metodu ucun 2");
+            Console.WriteLine("VowelLetter  metodu ucun 3");
+            int num = Convert.ToInt32(Console.ReadLine());
+            if (num==1)
+            {
+                NumLength();
+            }
+            else if (num==2)
+            {
+                Min();
+            }
+            else if (num==3)
+            {
+                VowelLetter();
+            }
+        }
+
+        static void Min()
+        {
+            int[] arr = {-9,7,12,22,30,67};
+            int small = 0;
+            small = arr[0];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (small > arr[i])
+                {
+                    small = arr[i];
+                }
+            }
+            Console.WriteLine("En kiçik element: " + small);
+        }
+
+        static void NumLength()
+        {
+            Console.WriteLine("eded daxil edin");
+            decimal num = Convert.ToInt64(Console.ReadLine());
+            int count = 0;
+            while (num>1)
+            {
+                num = num / 10;
+                count++;
+            }
+            Console.WriteLine(count);
+        }
+
+        static void VowelLetter()
+        {
+            Console.Write("Soz daxil edin : ");
+            string word = Convert.ToString(Console.ReadLine());
+            int sum = 0;
+
+            for (int i = 0; i < word.Length; i++)
+            {
+                if (word[i] == 'a'||word[i] == 'A'|| word[i] == 'o'|| word[i] == 'u' || word[i] == 'e' || word[i] == 'E' || word[i] == 'i' || word[i] == 'O' || word[i] == 'U' || word[i]=='I')
+                {
+                    sum++;
+                }
+
+            }
+            if (sum > 0)
+            {
+                Console.WriteLine(sum + "sait var");
+            }
+            else
+            {
+                Console.WriteLine("sait yoxdu");
+            }
+        }
+    }
+}
---
+++ Overload task/Overload task/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace Overload_task
+{
+    class Program
+    {
+      
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Area(4, -6, 6, 4));
+        }
+
+        static int Area(int radius)
+        //Çevrənin sahəsi
+        {
+            if (radius>0)
+            {
+                return radius * radius * 3;
+            }
+            else
+            {
+                return -1;
+            }
+        }
+
+        static int Area(int a, int b)
+           // Düzbucaqlının sahəsi
+        {
+            if (a>0 & b>0)
+            {
+                return a * b;
+            }
+            else
+            {
+                return -1;
+            }
+        }
+
+        static int Area(int a, int b, int c)
+        // Düzbucaqlı paralelpipedin tam səthinin sahəsi 
+        {
+            if (a>0 & b>0 & c>0)
+            {
+                return (a * b + a * c + b * c) / 2;
+            }
+            else
+            {
+                return -1;
+            }
+            
+        }
+
+        static int Area(int a, int b, int c, int r)
+        //Üçbucaqlının daxilinə çəkilmiş çevrənin sahəsi
+        {
+            if (a>0 & b>0 & c>0 & r>0)
+            {
+                int p = (a + b + c);
+                return p * r;
+            }
+            else
+            {
+                return -1;
+            }
+
+        }
+             
+    }
+}
+         

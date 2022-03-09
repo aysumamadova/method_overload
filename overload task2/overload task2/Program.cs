3c06767766ae9cf4402c0b5703ecbeee35bb42f9
Aysu Mammadova <tu7tw2d21@aztu.local>
2022-03-09 14:34:13 +0400

    add

---
+++ overload task2/overload task2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace overload_task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(HasLetter("tasklari yazin","a"));
+        }
+
+        static bool HasLetter(string text)
+        {
+            char letter = 'a';
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (text[i] == letter)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+
+
+        static bool HasLetter(string text,string word)
+        {
+          
+            for (int i = 0; i < text.Length; i++)
+            {
+                for (int j = 0; j < word.Length; j++)
+                {
+                    if (text[i]==word[j])
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+    }
+    
+}
